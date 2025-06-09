d4ce2c36db2d201fecf1f2de0f1f0808a54bfa3a
MOomoz401 <39644692+Momoz401@users.noreply.github.com>
2025-06-09 17:34:02 +0800

    8次测试自定义

--- extra-addons/new/__manifest__.py
+++ extra-addons/new/__manifest__.py
@@ -5,7 +5,9 @@
     'summary': 'Odoo 18 最简自定义模块测试',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
         'views/new_model_views.xml',
+
     ],
     'installable': True,
     'application': True,
